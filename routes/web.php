9c6a92922f4527be64cc7afc8a3e55d469735061
gpijuanc <gpijuanc@uoc.edu>
2025-12-01 20:17:39 +0100

    Web.php para crear les taules

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/setup-database-force', function () {
+    try {
+        // 1. Borrar y crear tablas
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        $migrateOutput = Artisan::output();
+
+        // 2. Poblar datos (Seeders)
+        Artisan::call('db:seed', ['--force' => true]);
+        $seedOutput = Artisan::output();
+
+        return "<h1>Base de datos configurada correctamente</h1>
+                <h3>Migrate Output:</h3><pre>$migrateOutput</pre>
+                <h3>Seed Output:</h3><pre>$seedOutput</pre>";
+    } catch (\Exception $e) {
+        return "<h1>Error</h1><pre>" . $e->getMessage() . "</pre>";
+    }
 });
