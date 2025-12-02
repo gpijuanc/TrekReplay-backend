67e13dc7443cf86696c75d36a749aa0e7173f682
gpijuanc <gpijuanc@uoc.edu>
2025-12-02 21:18:36 +0100

    web.php error 7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Intentem recuperar l'Authorization si Apache l'ha mogut a REDIRECT_...
+        if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+            $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+        }
+        
+        // O si l'ha mogut a una altra variable
+        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            $_SERVER['HTTP_AUTHORIZATION'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
+        } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+            $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+        }
     }
 }
