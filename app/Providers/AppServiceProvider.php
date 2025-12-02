f020091c6b5c87616fee356259a58d8f01649b4c
gpijuanc <gpijuanc@uoc.edu>
2025-12-02 21:50:10 +0100

    web.php error 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,16 +19,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Intentem recuperar l'Authorization si Apache l'ha mogut a REDIRECT_...
-        if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
-            $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+        // 1. Si la capçalera Authorization s'ha perdut, la recuperem
+        if (empty($_SERVER['HTTP_AUTHORIZATION'])) {
+            
+            // A vegades Apache la mou a REDIRECT_HTTP_AUTHORIZATION
+            if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+            } 
+            // O la posa a les variables d'autenticació PHP
+            elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+                $_SERVER['HTTP_AUTHORIZATION'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
+            } 
+            elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+                $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+            }
         }
-        
-        // O si l'ha mogut a una altra variable
-        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
-            $_SERVER['HTTP_AUTHORIZATION'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
-        } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
-            $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
-        }
-    }
-}
+    }}
