bbf4dccd1c187c1f7323580e2a531b421472c692
gpijuanc <gpijuanc@uoc.edu>
2025-12-20 10:51:46 +0100

    URL per treballar en local

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,15 @@ use App\Http\Controllers\CarretVirtualController;
 // Rutes públiques (no necessiten token)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/viatges', [ViatgeController::class, 'index']);
+Route::get('/viatges/{viatge}', [ViatgeController::class, 'show']);
 
 // Rutes protegides (necessiten token)
 Route::middleware('auth:sanctum')->group(function () {
     
-    Route::apiResource('viatges', ViatgeController::class);
+    Route::post('/viatges', [ViatgeController::class, 'store']);
+    Route::put('/viatges/{viatge}', [ViatgeController::class, 'update']);
+    Route::delete('/viatges/{viatge}', [ViatgeController::class, 'destroy']);
     Route::post('/generar-enllac', [ViatgeController::class, 'generarEnllacAfiliat']);
     Route::post('/viatges/{viatge}/upload-principal', [ViatgeController::class, 'uploadImatgePrincipal']); 
     Route::post('/viatges/{viatge}/upload-foto', [ViatgeController::class, 'uploadFotoGaleria']); 
